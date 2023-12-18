56dad39d0656f6b4c330ad949b3e544e5d6fe4b3
Elaidzha1940 <eli080293@gmail.com>
2023-12-18 15:31:30 +0300

    StunningLoading

---
+++ StunningLoading/StunningLoadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StunningLoadingApp.swift
+//  StunningLoading
+//
+//  Created by Elaidzha Shchukin on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct StunningLoadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
