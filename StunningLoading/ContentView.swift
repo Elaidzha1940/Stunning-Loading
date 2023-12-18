1c88d2c002b355c91b1b5a32c79b6bbf3058a669
Elaidzha1940 <eli080293@gmail.com>
2023-12-18 17:34:19 +0300

    Update ContentView

--- StunningLoading/ContentView.swift
+++ StunningLoading/ContentView.swift
@@ -15,20 +15,49 @@ struct ContentView: View {
     let timing: Double
     let maxCounter = 3
     let frame: CGSize
-    let Color: Color
+    let primaryColor: Color
     
-    init(color: Color = .white, size: CGFloat = 90 speed: Double = 0.5) {
+    init(color: Color = .white, size: CGFloat = 90, speed: Double = 0.5) {
         timing = speed * 2
+        frame = CGSize(width: size, height: size)
+        primaryColor = color
     }
     
     var body: some View {
         
-        VStack {
-         
+        ZStack {
+            Circle()
+                .fill(primaryColor)
+                .frame(height: frame.height / 3)
+                .offset(
+                    x: 0,
+                    y: isLoading ? -frame.height / 3 : 0)
+            
+            Circle()
+                .fill(primaryColor)
+                .frame(height: frame.height / 3)
+                .offset(
+                    x: isLoading ? -frame.height / 3 : 0,
+                    y: isLoading ? frame.height / 3 : 0)
+            
+            Circle()
+                .fill(primaryColor)
+                .frame(height: frame.height / 3)
+                .offset(
+                    x: isLoading ? frame.height / 3 : 0,
+                    y: isLoading ? frame.height / 3 : 0)
+        }
+        .animation(Animation.easeInOut(duration: timing), value: .repeatForever(autoreverses: true))
+        .frame(width: frame.width, height: frame.height, alignment: .center)
+        .rotationEffect(Angle(degrees: isLoading ? 360: 0))
+        .animation(Animation.easeInOut(duration: timing), value: .repeatForever(autoreverses: false))
+        .onAppear{
+            isLoading = true
         }
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
